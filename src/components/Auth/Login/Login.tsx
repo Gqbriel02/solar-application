cb2dd2d75dbca8f2bb2fd3f1a03ede019fae616c
Gqbriel02 <gabrielionita2002@gmail.com>
2024-05-15 13:59:40 +0300

    login page

---
+++ src/components/Auth/Login/Login.tsx
@@ -0,0 +1,143 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
+import { resetPassword, signIn } from '../../../services/AuthService';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthContext';
+
+function Copyright(props: any) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                SOLAR
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+function Login() {
+    const [userCredentials, setUserCredentials] = useState<{[key: string]: string}>({});
+    const { setIsAuthenticated } = useContext(AuthContext);
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    function handleCredentials(e: ChangeEvent<HTMLInputElement>) {
+        setUserCredentials({...userCredentials, [e.target.name]: e.target.value});
+        //console.log(userCredentials);
+    }
+
+    async function handleLogin(e: React.FormEvent<HTMLFormElement>) : Promise<void> {
+        e.preventDefault();
+        const { email, password } = userCredentials;
+        if (!email) {
+            setError('Please enter your email.');
+            return;
+        }
+        if (!password) {
+            setError('Please enter your password.');
+            return;
+        }
+
+        try {
+            await signIn(email, password, setError, setIsAuthenticated);
+            setError('');
+            navigate('/');
+        } catch (error) {
+            setError('Invalid email or password.');
+        }
+    }
+
+    function handlePasswordReset() {
+        resetPassword();
+    }
+
+    return (
+
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <Box component="form" onSubmit={handleLogin} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        onChange={handleCredentials}
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        onChange={handleCredentials}
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >
+                        Sign In
+                    </Button>
+
+                    {
+                        error &&
+                        <div  className="error">
+                            {error}
+                        </div>
+                    }
+
+                    <Grid container>
+                        <Grid item xs>
+                            <Link onClick={handlePasswordReset} variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/register" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+    );
+}
+
+export default Login;
