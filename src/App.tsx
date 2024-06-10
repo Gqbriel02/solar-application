04a1e6937d4c3be613e05c24d310716b25a78cb8
Gqbriel02 <gabrielionita2002@gmail.com>
2024-06-10 11:51:56 +0300

    added reports route

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
-import React, { useContext } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import React, {useContext} from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Login from './components/Auth/Login/Login';
 import Register from './components/Auth/Register/Register';
-import { Navbar } from './components/Navbar/Navbar';
-import { AuthContext } from './context/AuthContext';
+import {Navbar} from './components/Navbar/Navbar';
+import {AuthContext} from './context/AuthContext';
 import Content from "./components/Content/Content";
+import Reports from "./components/Reports/Reports";
+import DetailedReport from './components/Reports/DetailedReport/DetailedReport';
 
 function App() {
-    const { isAuthenticated } = useContext(AuthContext);
+    const {isAuthenticated} = useContext(AuthContext);
     return (
         <>
-            {isAuthenticated && <Navbar />}
+            {isAuthenticated && <Navbar/>}
 
             <Routes>
-                <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <Login />} />
+                <Route path="/login" element={isAuthenticated ? <Navigate to="/"/> : <Login/>}/>
                 <Route
                     path="/register"
-                    element={isAuthenticated ? <Navigate to="/" /> : <Register />}
+                    element={isAuthenticated ? <Navigate to="/"/> : <Register/>}
                 />
-                <Route element={<Content />}>
+                <Route element={<Content/>}>
                     <Route
                         path="/"
-                        element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+                        element={isAuthenticated ? <Home/> : <Navigate to="/login"/>}
                     />
+                    <Route
+                        path="/reports"
+                        element={isAuthenticated ? <Reports/> : <Navigate to="/login"/>}
+                    />
+                    <Route path="/reports/:id" element={isAuthenticated ? <DetailedReport/> : <Navigate to="/login"/>}/>
                 </Route>
             </Routes>
 
