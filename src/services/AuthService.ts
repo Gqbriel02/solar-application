b8c6976aa5f1d569427d60f175420d7c199bd8d4
Gqbriel02 <gabrielionita2002@gmail.com>
2024-06-18 00:25:23 +0300

    Added sign out method

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,7 +1,8 @@
 import {
     sendPasswordResetEmail,
     signInWithEmailAndPassword,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signOut
 } from 'firebase/auth';
 import {auth} from '../config/Firebase';
 
@@ -9,12 +10,11 @@ import {auth} from '../config/Firebase';
 export async function signIn(email: string, password: string, setError: (error: string) => void, setIsAuthenticated: (authenticated: boolean) => void): Promise<void> {
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        console.log(userCredential.user);
-        /*await signInWithEmailAndPassword(auth, email, password);*/
+        /*console.log('signIn usercredential: ', userCredential.user);*/
         setIsAuthenticated(true);
         return Promise.resolve();
     } catch (error) {
-        /*console.log("EROAREAAA: ", error);*/
+        /*console.log("Eroarea: ", error);*/
         return Promise.reject();
     }
 }
@@ -23,8 +23,8 @@ export function signUp(userCredentials: { [key: string]: string }, setError: (er
     return new Promise((resolve, reject) => {
         createUserWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
             .then((userCredential) => {
-                console.log(userCredential.user);
-                console.log('Registered successfully');
+                /*console.log(userCredential.user);
+                console.log('Registered successfully');*/
                 resolve();
             })
             .catch((error) => {
@@ -36,10 +36,31 @@ export function signUp(userCredentials: { [key: string]: string }, setError: (er
 
 export function resetPassword(): void {
     const email = prompt('Please enter your email:');
-    if (email !== null && email !== '') {
-        sendPasswordResetEmail(auth, email);
-        alert("Email sent! Check your inbox!");
+    if (email === null) {
+        return;
+    }
+    if (email !== '') {
+        sendPasswordResetEmail(auth, email)
+            .then(() => {
+                alert("Email sent! Check your inbox!");
+            })
+            .catch((error) => {
+                alert("Error: " + error.message);
+            });
     } else {
         alert("Invalid email address. Please provide a valid email.");
     }
+}
+
+export async function signOutUser(): Promise<void> {
+    try {
+        await signOut(auth);
+        return Promise.resolve();
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+export function getCurrentUser() {
+    return auth.currentUser;
 }
