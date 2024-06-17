fb656b44c6f049896fb3f05356664d7fb02aa179
Gqbriel02 <gabrielionita2002@gmail.com>
2024-06-18 00:37:57 +0300

    Added loading and current user

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,20 +1,48 @@
-import React, { createContext, useState } from 'react';
+import React, {createContext, useEffect, useState} from 'react';
+import {auth} from '../config/Firebase';
 
 export const AuthContext = createContext<AuthContextType>({
     isAuthenticated: false,
-    setIsAuthenticated: () => {}
+    setIsAuthenticated: () => {
+    },
+    currentUser: null,
+    setCurrentUser: () => {
+    },
+    loading: true
 });
 
 interface AuthContextType {
     isAuthenticated: boolean;
     setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+    currentUser: any;
+    setCurrentUser: React.Dispatch<React.SetStateAction<any>>;
+    loading: boolean;
 }
 
-const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const AuthProvider: React.FC<{ children: React.ReactNode }> = ({children}) => {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+    const [currentUser, setCurrentUser] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            if (user) {
+                /*console.log('e logat')*/
+                setIsAuthenticated(true);
+                setCurrentUser(user);
+            } else {
+                /*console.log('nu mai e logat')*/
+                setIsAuthenticated(false);
+                setCurrentUser(null);
+            }
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+        <AuthContext.Provider value={{isAuthenticated, setIsAuthenticated, currentUser, setCurrentUser, loading}}>
             {children}
         </AuthContext.Provider>
     );
