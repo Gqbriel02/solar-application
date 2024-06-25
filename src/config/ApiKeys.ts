fd83912df12a7303dc6f53269d70ddd551273513
Gqbriel02 <gabrielionita2002@gmail.com>
2024-06-25 19:00:15 +0300

    config files

---
+++ src/config/ApiKeys.ts
@@ -0,0 +1,6 @@
+const weatherApiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+const googleMapsKey: string = process.env.REACT_APP_GOOGLE_MAPS_API_KEY || '';
+
+
+export {weatherApiKey, googleMapsKey};
